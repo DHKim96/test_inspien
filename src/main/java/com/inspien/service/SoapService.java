a783540d3a386b8558fad13c75ba27d0d6dcff9c
DHKim96 <history8549@gmail.com>
2024-12-17 13:21:59 +0900

    fix. soap 메시지 생성 로직 수정

--- src/main/java/com/inspien/service/SoapService.java
+++ src/main/java/com/inspien/service/SoapService.java
@@ -1,10 +1,18 @@
 package com.inspien.service;
 
 import com.inspien.model.dto.User;
+import jakarta.xml.soap.SOAPException;
+import jakarta.xml.soap.SOAPMessage;
 
 public interface SoapService {
 
     String requestSoap(User user);
 
-    void parseSoapXML(String soapxml);
+    void parseSoapXML(String response) throws SOAPException;
+
+    void createSoapEnvelope(SOAPMessage soapMessage, User user) throws SOAPException;
+
+    String requestSoapWebService(User user) throws SOAPException;
+
+    SOAPMessage createSoapRequest(String soapAction, User user) throws SOAPException;
 }
--- src/main/java/com/inspien/service/SoapServiceImpl.java
+++ src/main/java/com/inspien/service/SoapServiceImpl.java
@@ -1,6 +1,7 @@
 package com.inspien.service;
 
 import com.inspien.model.dto.User;
+import jakarta.xml.soap.*;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -11,84 +12,93 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.soap.*;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringReader;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Base64;
 import java.util.Properties;
 
 public class SoapServiceImpl implements SoapService {
 
+    private final String endPoint;
+    private final String nameSpace = "in";
+    private final String nameSpaceURI = "http://inspien.co.kr/Recruit/Test";
+
+    public SoapServiceImpl() {
+        Properties prop = new Properties();
+        InputStream input = getClass().getClassLoader().getResourceAsStream("hosts.properties");
+        try {
+            prop.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("hosts.properties 를 읽어올 수 없습니다.");
+        }
+        String inspienpoc = prop.getProperty("soap.service.host");
+        String port = prop.getProperty("soap.service.port");
+        endPoint = "http://" + inspienpoc + ":" + port + "/XISOAPAdapter/MessageServlet?senderParty=&senderService=INSPIEN&receiverParty=&receiverService=&interface=InspienGetRecruitingTestServicesInfo&interfaceNamespace=http%3A%2F%2Finspien.co.kr%2FRecruit%2FTest";
+    }
+
     @Override
     public String requestSoap(User user) {
 
         try {
-            Properties prop = new Properties();
-            InputStream input = getClass().getClassLoader().getResourceAsStream("hosts.properties");
-            prop.load(input);
 
-            String inspienpoc = prop.getProperty("soap.service.host");
-            String port = prop.getProperty("soap.service.port");
-            String endPoint = "http://" + inspienpoc + ":" + port + "/XISOAPAdapter/MessageServlet?senderParty=&senderService=INSPIEN&receiverParty=&receiverService=&interface=InspienGetRecruitingTestServicesInfo&interfaceNamespace=http%3A%2F%2Finspien.co.kr%2FRecruit%2FTest";
+            URL url = new URL(endPoint);
 
-            // 1. soap 메시지 생생
-            MessageFactory messageFactory = MessageFactory.newInstance(); // 싱글톤?
-            SOAPMessage soapMessage = messageFactory.createMessage();
-            SOAPPart soapPart = soapMessage.getSOAPPart();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true); // 안해줬더니 에러 발생
 
-            // 2. SOAP 구조 구성
-            SOAPEnvelope soapEnvelope = soapPart.getEnvelope();
-            soapEnvelope.addNamespaceDeclaration("temp", "http://inspien.co.kr/Recruit/Test");
-            SOAPBody body = soapEnvelope.getBody();
+            conn.setRequestMethod("POST");
+            conn.addRequestProperty("Content-Type", "text/xml");
 
-            // 3. Operation 및 파라미터 추가
+            String sendMessage = """
+                    <?xml version="1.0" encoding="UTF-8"?>
+                    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
+                                    xmlns:ins="http://inspien.co.kr/Recruit/Test">
+                        <soap:Body>
+                            <ins:MT_RecruitingTestServices>
+                                <ins:NAME>%s</ins:NAME>
+                                <ins:PHONE_NUMBER>%s</ins:PHONE_NUMBER>
+                                <ins:E_MAIL>%s</ins:E_MAIL>
+                            </ins:MT_RecruitingTestServices>
+                        </soap:Body>
+                    </soap:Envelope>
+                    """.formatted(user.getName(), user.getPhone(), user.getEmail());
 
-            SOAPElement operation = body.addChildElement("MT_RecruitingTestServices", "temp");
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(sendMessage);
+            wr.flush();
 
-            SOAPElement nameElement = operation.addChildElement("NAME", "temp");
-            nameElement.addTextNode(user.getName());
+            // 결과 읽기
+            String inputLine = null;
+            StringBuilder response = new StringBuilder();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
 
-            SOAPElement phoneElement = operation.addChildElement("PHONE_NUMBER", "temp");
-            phoneElement.addTextNode(user.getPhone());
 
-            SOAPElement emailElement = operation.addChildElement("E_MAIL", "temp");
-            emailElement.addTextNode(user.getEmail());
+            System.out.println("response :" + response);
 
-            soapMessage.saveChanges();
+            in.close();
+            wr.close();
+            conn.disconnect();
 
-            // 4. SOAP request
-            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
-            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
-
-            System.out.println("Request SOAP service at :" + endPoint);
-
-            SOAPMessage response = soapConnection.call(soapMessage, endPoint);
-            
-            // 5. SOAP response 처리
-            System.out.println("Response SOAP Message : ");
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            response.writeTo(outputStream); // SOAPBody 전체를 출력
-            String res = outputStream.toString("UTF-8");
-            soapConnection.close();
-            return res;       // 문자열로 변환
+            return response.toString();
         } catch (Exception e) {
-            e.printStackTrace();
             throw new RuntimeException("SOAP 요청 실패: " + e.getMessage());
         }
     }
 
     @Override
-    public void parseSoapXML(String soapXmlData) {
+    public void parseSoapXML(String response) {
 
 
         try {
 
             InputSource is = new InputSource();
-            is.setCharacterStream(new StringReader(soapXmlData));
+            is.setCharacterStream(new StringReader(response));
             is.setEncoding("UTF-8");
-            
+
             // XML 파싱
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
@@ -122,6 +132,88 @@ public class SoapServiceImpl implements SoapService {
         }
     }
 
+    @Override
+    public void createSoapEnvelope(SOAPMessage soapMessage, User user) {
+        SOAPPart soapPart = soapMessage.getSOAPPart();
+        try {
+            // SOAP Envelope 생성
+            SOAPEnvelope envelope = soapPart.getEnvelope();
+            envelope.addNamespaceDeclaration(nameSpace, nameSpaceURI);
+            
+            // SOAP BODY 생성
+            SOAPBody body = envelope.getBody();
+            SOAPElement operation = body.addChildElement("MT_RecruitingTestServices", "in");
+
+            // 파라미터 추가
+            operation.addChildElement("NAME", "in").addTextNode(user.getName());
+            operation.addChildElement("PHONE_NUMBER", "in").addTextNode(user.getPhone());
+            operation.addChildElement("E_MAIL", "in").addTextNode(user.getEmail());
+
+        } catch (SOAPException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String requestSoapWebService(User user) {
+        try {
+            
+            // SOAP 커넥션 생성
+            SOAPConnectionFactory factory = SOAPConnectionFactory.newInstance();
+            SOAPConnection soapConnection = factory.createConnection();
+            
+            // SOAP 메시지 전송
+
+            SOAPMessage request = this.createSoapRequest("http://sap.com/xi/WebService/soap1.1", user);
+            SOAPMessage response = soapConnection.call(request, endPoint);
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            response.writeTo(outputStream);
+            String res = outputStream.toString("UTF-8");       // 문자열로 변환
+
+            soapConnection.close();
+            outputStream.close();
+
+            return res;
+        } catch (SOAPException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public SOAPMessage createSoapRequest(String soapAction, User user) {
+
+        try {
+            MessageFactory messageFactory = MessageFactory.newInstance();
+
+            SOAPMessage soapMessage = messageFactory.createMessage();
+            
+            //  BODY 생성
+            this.createSoapEnvelope(soapMessage, user);
+
+            // Header 생성(soapAction 존재)
+            MimeHeaders headers = soapMessage.getMimeHeaders();
+            headers.addHeader("SOAPAction", soapAction);
+
+            soapMessage.saveChanges();
+
+            System.out.println("Request SOAP Message:");
+            soapMessage.writeTo(System.out);
+            System.out.println("\n");
+
+            return soapMessage;
+        } catch (SOAPException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private String getTagValue(Document doc, String tagName) {
         NodeList nodelist = doc.getElementsByTagName(tagName);
 
@@ -131,8 +223,8 @@ public class SoapServiceImpl implements SoapService {
 
         Element e = null;
 
-        for(int i=0; i< nodelist.getLength(); i++) {
-            e = (Element)nodelist.item(i);
+        for (int i = 0; i < nodelist.getLength(); i++) {
+            e = (Element) nodelist.item(i);
             res.append(e.getTextContent());
         }
 
